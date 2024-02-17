b2a70dc1736fda9b5d4c05fd520c101d8054c8f5
pranavvsdev <149656049+pranavvsdev@users.noreply.github.com>
2024-02-17 16:31:54 +0530

    Update approveDeposit.js

--- scripts/approveDeposit.js
+++ scripts/approveDeposit.js
@@ -1,39 +1,48 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const fxRootContractABI = require("../fxRootContractABI.json");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
-
-const tokenAddress = ""; // place your erc20 contract address here
-const tokenABI = tokenContractJSON.abi;
-const fxERC20RootAddress = "0x3658ccFDE5e9629b0805EB06AaCFc42416850961";
-const walletAddress = ""; // place your public address for your wallet here
+const { ethers } = require("hardhat");
+const { FXRootContractAbi } = require('../artifacts/FXRootContractAbi.js');
+const ABI = require('../artifacts/contracts/cindelsa.sol/cindelsa.json');
+require('dotenv').config();
 
 async function main() {
+//goerli node
+  const networkAddress = 'https://ethereum-goerli.publicnode.com';
+  const privateKey = process.env.PRIVATE_KEY;
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
-    const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-    const fxContract = await hre.ethers.getContractAt(fxRootContractABI, fxERC20RootAddress);
+  const wallet = new ethers.Wallet(privateKey, provider);
 
-    const approveTx = await tokenContract.approve(fxERC20RootAddress, 500);
-    await approveTx.wait();
+  const [signer] = await ethers.getSigners();
+//contract
+  const Cindelsa = await ethers.getContractFactory("Cindelsa");
+  const cindelsa = await Cindelsa.attach('0x001D57CDBAfB4804106efDe5fD063211EA0Bcddb');
+//fxroot for transfer
+  const fxRootAddress = '0xF9bc4a80464E48369303196645e876c8C7D972de';
+  const fxRoot = await ethers.getContractAt(FXRootContractAbi, fxRootAddress);
 
-    console.log('Approval confirmed');
+  const approveTx = await cindelsa.connect(signer).setApprovalForAll(fxRootAddress, true);
+  await approveTx.wait();
+  console.log('The Assets have been approved for transfer');
+//mapping each nft to id
+  const Identifier = [0,1, 2, 3, 4]; 
+  for (let i = 0; i <Identifier.length; i++) {
+    const depositTx = await fxRoot.connect(signer).deposit(
+      cindelsa.address,
+      wallet.address, 
+      Identifier[i],
+      '0x6566',
+      { gasLimit: 100000 }
+    );
 
-
-    const depositTx = await fxContract.deposit(tokenAddress, walletAddress, 500, "0x6556");
     await depositTx.wait();
-
-    console.log("Tokens deposited");
-  
   }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+//Confirmation
+  console.log("The Nfts are successfully deposited to polygon mumbai test network");
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
-    process.exitCode = 1;
+    process.exit(1);
   });
